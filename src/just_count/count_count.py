9f92260a752c5d0493e7942d300e4418d2f3a3c8
Juaopteinde <143963603+Juaopteinde@users.noreply.github.com>
2024-11-15 09:52:20 +0100

    Added click commands

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,8 +1,11 @@
 from just_count.square import square
+import click
 
 
-def main():
-    print(f"The square of 5 is {square(5)}")
+@click.command()
+@click.argument("number", default=5)
+def main(number):
+    print(f"The square of {number} is {square(number)}")
 
 
 if __name__ == "__main__":
